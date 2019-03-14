3851245e5e6f3d145e8b768df88f42a5592615fd
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-03-14 10:09:48 +0100

    Clean up the class Movie

--- movies/src/main/java/com/example/movies/Movie.java
+++ movies/src/main/java/com/example/movies/Movie.java
@@ -19,39 +19,5 @@ public class Movie {
     private String releaseDate;
     private String country;
     private String imdbRating;
-
-    /*private final static String url = "http://www.omdbapi.com/?apikey=43576692&i=";
-    private final static RestTemplate restTemplate = new RestTemplate();*/
-
-
-    /*public Movie(String movieId, boolean haveSeen, boolean wantToSee) throws JSONException {
-
-        JSONObject obj = new JSONObject(restTemplate.getForObject(url + movieId, String.class));
-
-        if (obj.getBoolean("Response")) {
-
-            this.movieId = movieId;
-            this.haveSeen = haveSeen;
-            this.wantToSee = wantToSee;
-
-            this.title = obj.getString("Title");
-            this.director = obj.getString("Director");
-            this.actors = obj.getString("Actors");
-            this.runTime = obj.getString("Runtime");
-            this.genre = obj.getString("Genre");
-            this.releaseDate = obj.getString("Released");
-            this.country = obj.getString("Country");
-            this.imdbRating = obj.getString("imdbRating");
-
-        }
-
-        else {
-
-            throw new MovieException("No movie found with id: " + movieId);
-
-        }
-
-    }*/
-
-
+    
 }
